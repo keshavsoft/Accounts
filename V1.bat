05c9992f4b82cf44dfa8307fde4bcbff52cc867c
krishnasanjay27 <Krishnasanjaybangaru@gmail.com>
2025-07-11 17:33:19 +0530

    created with table

---
+++ V1.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set myvar=
+echo %myvar%
+call :myfunction myvar
+echo %myvar%
+mkdir Public\%myvar%
+
+for %%f in (Schemas\*) do (
+    echo %%f
+    )
+
+:myFunction
+setlocal enabledelayedexpansion
+set "max=0"
+for /d %%G in (Public\V*) do (
+    set "folder=%%~nxG"
+    set "ver=!folder:V=!"
+    if !ver! gtr !max! set "max=!ver!"
+)
+set /a NEXT_VERSION=max+1
+
+endlocal & set "NEXT_VERSION=%NEXT_VERSION%"
+set %1=V%NEXT_VERSION%
+EXIT /B 0
